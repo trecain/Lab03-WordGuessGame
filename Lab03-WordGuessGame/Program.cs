6ebf23276138b47ac240fc4dfc1a6de1cfde7890
trecain <developedbycain@yahoo.com>
2018-10-14 21:47:52 -0500

    yay, got a useless menu to show up on the console hahaha

--- Lab03-WordGuessGame/Program.cs
+++ Lab03-WordGuessGame/Program.cs
@@ -9,6 +9,7 @@ namespace Lab03_WordGuessGame
         {
             Console.WriteLine("Hello World!");
             OpenWordBank();
+            UserInterfaceMenu();
         }
 
         static void OpenWordBank()
@@ -30,5 +31,28 @@ namespace Lab03_WordGuessGame
             }
         }
 
+        static public int UserInterfaceMenu()
+        {
+            try
+            {
+                Console.WriteLine("Hello contestant!, Welcome to Word Guess Game.");
+                Console.WriteLine("1: New Game");
+                Console.WriteLine("2: Add A Word");
+                Console.WriteLine("3: View Words");
+                Console.WriteLine("4: Delete Words");
+                Console.WriteLine("5: Exit Game");
+                int userMenuInput = Convert.ToInt32(Console.ReadLine());
+                return userMenuInput;
+
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Good Luck Contestant!");
+            }
+        }
     }
 }
