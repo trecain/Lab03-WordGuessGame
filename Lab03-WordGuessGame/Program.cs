3e00cb88685d2b058d65764bf9af49c9ab3386a7
trecain <developedbycain@yahoo.com>
2018-10-14 21:35:53 -0500

    figured out how to create a text file

--- Lab03-WordGuessGame/Program.cs
+++ Lab03-WordGuessGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab03_WordGuessGame
 {
@@ -7,8 +8,27 @@ namespace Lab03_WordGuessGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            OpenWordBank();
         }
 
+        static void OpenWordBank()
+        {
+            string path = @"C:\Users\ercai\Desktop\codefellows\401\Lab03-WordGuessGame\BankOfWords.txt";
+
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path))
+                {
+                    Byte[] fileText = new System.Text.UTF8Encoding(true).GetBytes("Hello guest, welcome to word guessing game.");
+                    fs.Write(fileText, 0, fileText.Length);
+                }
+            }
+            else
+            {
+                string[] fileText = File.ReadAllLines(path);
+                int fileTextLength = fileText.Length;
+            }
+        }
 
     }
 }
